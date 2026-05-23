5b02435783c373db0129fec2baeb96fba8746c7d
Ashutosh Awasthi <ashutoshawasthi66@gmail.com>
2026-05-23 09:10:02 +0530

    fix dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,8 @@ module com.example.sudoku {
     requires javafx.controls;
     requires javafx.fxml;
     requires de.jensd.fx.glyphs.fontawesome;
+    requires de.jensd.fx.glyphs.commons;
     requires java.sql;
-
 
     opens com.example.sudoku to javafx.fxml;
     exports com.example.sudoku;
