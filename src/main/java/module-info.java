6264296fda7f6d116c74b587730639213147333c
Ashish Kumar Singh <ashishkumarsingh@Ashishs-Mac-mini.local>
2026-09-24 03:46:20 +0530

    Updated the Java and JavaFx version

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.sudoku {
     requires javafx.fxml;
     requires de.jensd.fx.glyphs.fontawesome;
     requires java.sql;
+    requires mysql.connector.j;
 
 
     opens com.example.sudoku to javafx.fxml;
